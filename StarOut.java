b9b844b1296caddda2815238504ac896ef930e5f
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-27 23:48:26 +0530

    Simple Star Using sout Print

---
+++ StarOut.java
@@ -0,0 +1,11 @@
+public class StarOut {
+    public static void main(String args[]){
+        //Simple pattern Print Using System Print
+
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
