a9151fa45eb82134b556c91e74490ac4cbe25072
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-27 23:42:38 +0530

    Added New line

--- JavaBasics.java
+++ JavaBasics.java
@@ -2,5 +2,11 @@ public class JavaBasics {
     public static void main(String args[]){
         //BoilerPlate Code and Hello World Program :)
         System.out.println("Hello World");
+
+        System.out.print("Sagar");
+        System.out.print("Dhadke \n");
+
+        System.out.print("Android Developer \n");
+        System.out.print("I Learn Currently Java DSA and Jetpack Compose:)");
     }
 }
