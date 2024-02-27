60089a838d1285c4fab3fe1871508ad325b5fd25
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-27 23:44:47 +0530

    Comments in Java

---
+++ Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String args[]){
+        //Single Line Comment 
+
+        /*
+         * This 
+         * Is 
+         * Multi-line
+         * Comment
+         */
+    }
+}
