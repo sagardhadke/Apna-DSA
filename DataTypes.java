80cd4c555344db9a3625627e956361e548009356
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-27 23:58:33 +0530

    All DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String args[]){
+        //DataTypes in Java 
+
+        //Primitive
+        int a = 210210;
+        float b = 20.23f;
+        double c = 56.35;
+        short d = 6461;
+        long e = 4785241351351316464L;
+        char f = 'S';
+        byte g = 124;
+        boolean h = true;
+
+        //Non-Primitive
+        String name = "Sagar";
+        /* 
+         * Arrays
+         * Class
+         * Object
+         * Interface
+         */
+    }
+}
