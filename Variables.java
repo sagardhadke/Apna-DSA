15ba28e31a1e0a99a3650c921a0fccc145ed44da
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-27 23:52:24 +0530

    Variables in Java

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        //variables in Java
+        //store the values in container 
+
+        int a = 20;
+        String name = "Sagar Dhadke";
+        char firstLatter = 'S';
+
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(firstLatter);
+        name = "Sagar";
+
+        System.out.println(name);
+
+    }
+}
