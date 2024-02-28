475312c4156ec1b10be3686a4caf9473e315909e
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-28 19:43:32 +0530

    Types Conversion in Java

---
+++ TypesConversion.java
@@ -0,0 +1,25 @@
+public class TypesConversion {
+    public static void main(String args[]){
+
+        // Types Conversion in java
+
+        //in Types conversion values wil be change in right to left side all types
+        //Bytes => Short => int => Float => Long => Double
+
+        //Example 1
+        short a = 26546;
+        long b = a;
+        System.out.println(a);
+
+        //Example 2
+      /*  int c = 25;
+        short d = c;  //showing error message
+        System.out.println(d); */
+
+        //Example 3
+        long e = 56464634534343L;
+        double f = e;
+        System.out.println(f);
+
+    }
+}
