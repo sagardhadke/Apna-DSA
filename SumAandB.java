baa8d6c9d8b11874b4664dbe2db4f2054b1ac125
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-28 15:32:31 +0530

    Sum of A and B

---
+++ SumAandB.java
@@ -0,0 +1,11 @@
+public class SumAandB {
+    public static void main(String args[]){
+        //Sum of a and b
+
+        int a = 20;
+        int b = 65;
+        int sum = a+b;
+
+        System.out.println(sum);
+    }    
+}
