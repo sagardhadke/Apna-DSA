e910ce320a786a1f9066f8dd4192e53846d8cbdf
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-28 16:22:56 +0530

    All Inputes In Java

--- InputBasics.java
+++ InputBasics.java
@@ -14,5 +14,17 @@ public class InputBasics {
         System.out.println(name);
         System.out.println(age);
 
+        //All Inputs 
+
+        System.out.println(sc.next());  // show only first letter like "Sagar Dhadke"  Output => "Sagar"
+        System.out.println(sc.nextLine()); // show all String values
+        System.out.println(sc.nextInt()); // show all integer values
+        System.out.println(sc.nextByte());  // show all byte values
+        System.out.println(sc.nextFloat()); // show all float values
+        System.out.println(sc.nextDouble()); // show all Double values
+        System.out.println(sc.nextBoolean()); // show all Boolean 
+        System.out.println(sc.nextShort()); // show all Short
+        System.out.println(sc.nextLong()); // show all Long
+
     }
 }
