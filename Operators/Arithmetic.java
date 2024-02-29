d9aa678d38e2a04f9e73674c6b9965db532d405c
Sagar Dhadke <peaceunique2003@gmail.com>
2024-02-29 23:36:01 +0530

    Arithmetic Operators In Javas

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        //Arithmetic Operators
+        
+        int a = 25;
+        int b = 32;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+    }
+}
