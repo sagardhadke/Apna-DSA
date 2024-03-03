37c7384aa6738a14443a2ed94e81bce976f664f8
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-03 23:39:47 +0530

    Calculator Pro

---
+++ Project/Calculator.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //Project 1 Calculator Pro
+
+        System.out.println("Welcome To Sagar Calculator Pro");
+        
+
+    }   
+}
